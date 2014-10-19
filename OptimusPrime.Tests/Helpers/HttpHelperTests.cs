db28fdb5eba41caafeedc9e68f1d2a50f2535f88
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2014-10-19 23:41:37 +0200

    Started adding tests

---
+++ OptimusPrime.Tests/Helpers/HttpHelperTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using OptimusPrime.Helpers;
+using System.Linq;
+
+namespace OptimusPrime.Tests.Helpers
+{
+    [TestFixture]
+    public class HttpHelperTests
+    {
+        private HttpHelper _target;
+
+        [TestCase("", 0)]
+        [TestCase("Lorem ipsum dolor sit amet, consectetur adipiscing elit. http://www.lipsum.com", 1)]
+        [TestCase("http://www.lipsum.com Lorem ipsum dolor sit amet, consectetur adipiscing elit. http://www.lipsum.com/", 2)]
+        [TestCase("http://www.lipsum.com Lorem ipsum http://www.lipsum.com/ adipiscing elit. http://www.lipsum.com/", 3)]
+        [TestCase("http://www.lipsum.com http://www.lipsum.com http://www.lipsum.com http://www.lipsum.com", 4)]
+        public void ExtractUris_ShouldExtractUrisFromMessage(string message, int nbrUrls)
+        {
+            var uris = _target.ExtractUris(message);
+            Assert.AreEqual(nbrUrls, uris.Count());
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            _target = new HttpHelper();
+        }
+    }
+}
--- OptimusPrime/Helpers/HttpHelper.cs
+++ OptimusPrime/Helpers/HttpHelper.cs
@@ -8,7 +8,7 @@ using System.Web;
 
 namespace OptimusPrime.Helpers
 {
-    internal class HttpHelper : IHttpHelper
+    public class HttpHelper : IHttpHelper
     {
         private const string UrlRegExp =
             @"http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+";
